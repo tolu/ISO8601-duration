a100a3bc37e7bdd1f85529ca058dfe4c10c7d059
Torstein Bjørnstad <tosh@tosh.no>
2018-09-20 15:46:21 +0200

    Include missing default export in types

--- index.d.ts
+++ index.d.ts
@@ -12,3 +12,12 @@ export const pattern: RegExp;
 export function parse(durationString: string): Duration;
 export function end(duration: Duration, startDate?: Date): Date;
 export function toSeconds(duration: Duration, startDate?: Date): number;
+
+declare const _default: {
+  end: typeof end;
+  toSeconds: typeof toSeconds;
+  pattern: typeof pattern;
+  parse: typeof parse;
+};
+
+export default _default;
